4811197c6a963fa60e74ee4572f6ff9cd9bd23fc
thiagosilvadf <thiagothsc1603@outlook.com>
2025-05-23 17:02:41 -0300

    Alterado links

---
+++ scripts.js
@@ -0,0 +1,27 @@
+/*
+
+variaveis: Pedacinhos de memória do computador que posso colocar o que eu quiser
+
+Quando é nome coloca ""
+Console.log: Ferramento que mostra  na tela
+
+Função: Um pedacinho de código, que só é execultado quando é chamado
+
+Algorítmo: Passos para fazer algo, ex atravessar a rua
+
+Lógica de Programação: Saber fazer o algoritmo, saber os passos para criar o programa que você quer
+
+QuerySelector = Pega algo que eu quero no HTML
+
+Quando estamos no JavaScript e queremos chamar o HTML, usamos o document
+
+ Cores usadas: #017143; #eb7495; #d752b1.
+
+*/
+
+let circulo = document.querySelector(".circulo")
+
+function trocarCor(cor) {
+    circulo.style.background = cor
+}
+
